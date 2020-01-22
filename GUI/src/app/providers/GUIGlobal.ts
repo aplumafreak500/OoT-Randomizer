578080d5613eb3d17366fc8e35ee5eb32aad034e
Alex Pensinger <aplumafreak500@gmail.com>
2020-01-22 16:45:46 +0000

    change repo refs in GUI

--- GUI/src/app/providers/GUIGlobal.ts
+++ GUI/src/app/providers/GUIGlobal.ts
@@ -512,7 +512,7 @@ export class GUIGlobal {
 
         this.globalEmitter.emit({ name: "local_version_checked", version: res });
 
-        var remoteFile = await this.http.get("https://raw.githubusercontent.com/Roman971/OoT-Randomizer/Dev-R/version.py", { responseType: "text" }).toPromise();
+        var remoteFile = await this.http.get("https://raw.githubusercontent.com/aplumafreak500/OoT-Randomizer/master/version.py", { responseType: "text" }).toPromise();
 
         let remoteVersion = remoteFile.substr(remoteFile.indexOf("'") + 1);
         remoteVersion = remoteVersion.substr(0, remoteVersion.indexOf("'"));
